8c48392898cfe22437e8cb82e41303ec62d90c88
chrishoen <chris123hoen@gmail.com>
2019-07-02 13:22:01 -0400

    added strobe thread

--- CommonLib/cmnPriorities_linux.cpp
+++ CommonLib/cmnPriorities_linux.cpp
@@ -23,20 +23,7 @@ Priorities::Priorities()
 {
    mTsPrint           = Ris::Threads::Priority(-1, 20);
    mLogFile           = Ris::Threads::Priority(-1, 21);
-   mBinaryComm        = Ris::Threads::Priority( 3, 51);
-   mBinarySim         = Ris::Threads::Priority(-1, 35);
-   mSerialMsg         = Ris::Threads::Priority( 3, 80);
-   mGCodeComm         = Ris::Threads::Priority(-1, 60);
-   mSerialString      = Ris::Threads::Priority(-1, 80);
-   mGraphics          = Ris::Threads::Priority(-1, 40);
-   mScriptSeqLong     = Ris::Threads::Priority(-1, 50);
-   mScriptSeqShort    = Ris::Threads::Priority(-1, 70);
-
-   mControl           = Ris::Threads::Priority(-1, 40);
-   mControlUdp        = Ris::Threads::Priority(-1, 60);
-   mIsoch             = Ris::Threads::Priority(-1, 40);
-
-   mForceSerialRx     = Ris::Threads::Priority(-1, 80);
+   mStrobe            = Ris::Threads::Priority(-1, 95);
 }
 
 //******************************************************************************
---
+++ CommonLib/cxStrobeThread.cpp
@@ -0,0 +1,112 @@
+/*==============================================================================
+Description:
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+#include "stdafx.h"
+
+#include "risProgramTime.h"
+#include "risThreadsPriorities.h"
+#include "tsThreadServices.h"
+
+#include "cmnPriorities.h"
+#include "cxStrobeParms.h"
+
+#define  _CXSTROBETHREAD_CPP_
+#include "cxStrobeThread.h"
+
+namespace CX
+{
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+StrobeThread::StrobeThread()
+{
+   // Set base class variables.
+   BaseClass::setThreadName("Strobe");
+   BaseClass::setThreadPrintLevel(gStrobeParms.mStrobePrintLevel);
+
+   // Set base class variables.
+   BaseClass::setThreadPriority(Cmn::gPriorities.mStrobe);
+
+   // Set timer period.
+   BaseClass::mTimerPeriod = gStrobeParms.mTimerThreadPeriod;
+   mTimeMarker.initialize(100);
+
+   // Members
+   mTPFlag = false;
+   mTestCode = 1;
+   mTestCount = 0;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void StrobeThread::threadInitFunction()
+{
+   if (mTestCode !=3) return;
+
+   BaseClass::showThreadFullInfo();
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void StrobeThread::executeOnTimer(int aTimeCount)
+{
+   if (aTimeCount < 20) return;
+
+   switch (mTestCode)
+   {
+      case 1: executeTest1 (aTimeCount); break;
+      case 2: executeTest2 (aTimeCount); break;
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void StrobeThread::executeTest1(int aTimeCount)
+{
+   mTimeMarker.doUpdate();
+
+   if (mTimeMarker.mStatistics.mEndOfPeriod)
+   {
+//    TS::print(0, "CURRENT %10.4f", Ris::getCurrentProgramTime());
+      TS::print(0, "TEST1 %5d %1d %5d $$ %10.3f %10.3f %10.3f %10.3f $$ %10.3f",
+         mTestCount++,
+         BaseClass::getThreadProcessorNumber(),
+         mTimeMarker.mStatistics.mSize,
+         mTimeMarker.mStatistics.mMean,
+         mTimeMarker.mStatistics.mStdDev,
+         mTimeMarker.mStatistics.mMinX,
+         mTimeMarker.mStatistics.mMaxX,
+         mTimeMarker.mStatistics.mMaxX - mTimeMarker.mStatistics.mMinX);
+      mTimeMarker.mStatistics.mEndOfPeriod = false;
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void StrobeThread::executeTest2(int aTimeCount)
+{
+   Prn::print(Prn::ThreadRun1, "TEST2 %5d $$ %3d %10.4f",
+      aTimeCount,
+      BaseClass::getThreadProcessorNumber(),
+      Ris::getCurrentProgramTime());
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+}//namespace
