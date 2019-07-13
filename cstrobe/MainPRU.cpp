ef9fa3611134a9c875c9292a8115581d30216de4
chrishoen <chris123hoen@gmail.com>
2019-07-13 10:58:30 -0400

    MainPRU

--- cstrobe/MainPRU.cpp
+++ cstrobe/MainPRU.cpp
@@ -2,9 +2,10 @@
 
 #include "risThreadsProcess.h"
 
-#include "cisCProcDir.h"
-#include "risPortableCalls.h"
-#include "cxStrobeParms.h"
+#include "prussdrv.h"
+#include "pruss_intc_mapping.h"
+
+#define PRU_NUM 	0
 
 //******************************************************************************
 //******************************************************************************
@@ -18,6 +19,21 @@ void pru_initialize()
    //***************************************************************************
    // Initialize program resources.
 
+   int tRet = 0;
+
+   tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+
+   tRet = prussdrv_init();
+   printf("prussdrv_init          %d\n", tRet);
+
+   tRet = prussdrv_open(PRU_EVTOUT_0);
+   printf("prussdrv_open          %d\n", tRet);
+
+   tRet = prussdrv_pruintc_init(&pruss_intc_initdata);
+   printf("prussdrv_pruintc_init  %d\n", tRet);
+
+   tRet = prussdrv_exec_program(PRU_NUM, "./pru/PRU_gpioToggle.out");
+   printf("prussdrv_exec_program  %d\n", tRet);
 }
 
 //******************************************************************************
