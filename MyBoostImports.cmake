c7e64c29595ced20eb8c76f62245145ef040b219
chrishoen <chris123hoen@gmail.com>
2019-10-01 12:49:31 -0400

    adding boost

--- MyBoostImports.cmake
+++ MyBoostImports.cmake
@@ -2,17 +2,16 @@
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_init_global_import_variables)
+function(my_init_boost_import_variables)
 
-   if(MSVC)
-      set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/RisLib.lib" PARENT_SCOPE)
-   elseif(NOT CMAKE_SYSTEM_VERSION EQUAL 101)
-      set (MyRisLibIncludePath "/usr/local/include/RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "/usr/local/lib/libRisLib.a" PARENT_SCOPE)
+   if(NOT CMAKE_SYSTEM_VERSION EQUAL 101)
+      set (MyBoostIncludePath "/usr/local/include/boost" PARENT_SCOPE)
+      set (MyBoostSystemImportPath  "/usr/local/lib/libboost_system.a" PARENT_SCOPE)
+      set (MyBoostThreadImportPath  "/usr/local/lib/libboost_thread.a" PARENT_SCOPE)
    else()
-      set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/libRisLib.a" PARENT_SCOPE)
+      set (MyBoostIncludePath "C:/Beagle/boost/include" PARENT_SCOPE)
+      set (MyBoostSystemImportPath  "C:/Beagle/boost/lib/libboost_system.a" PARENT_SCOPE)
+      set (MyBoostThhreadImportPath  "C:/Beagle/boost/lib/libboost_thread.a" PARENT_SCOPE)
    endif()
 endfunction()
 
@@ -20,33 +19,22 @@ endfunction()
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_lib_import_RisLib _target)
+function(my_lib_import_boost _target)
 
-   add_library(RisLib STATIC IMPORTED)
+   add_library(boost_system STATIC IMPORTED)
+   add_library(boost_thread STATIC IMPORTED)
 
-   set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
+   set_target_properties(boost_system PROPERTIES IMPORTED_LOCATION ${MyBoostSystemImportPath})
+   set_target_properties(boost_thread PROPERTIES IMPORTED_LOCATION ${MyBoostThreadImportPath})
 
-   if (MSVC)
-      target_link_libraries(RisLib INTERFACE ws2_32)
-      target_link_libraries(RisLib INTERFACE winmm)
-   else()
-      if (CMAKE_SYSTEM_VERSION EQUAL 101)
-         set (MyPThreadImportPath  "C:/Beagle/toolchain/arm-linux-gnueabihf/lib/libpthread.so")
-      else()
-         set (MyPThreadImportPath  "/usr/lib/arm-linux-gnueabihf/libpthread.so")
-      endif()
-      add_library(PThreadLib SHARED IMPORTED)
-      set_target_properties(PThreadLib PROPERTIES IMPORTED_LOCATION ${MyPThreadImportPath})
-      target_link_libraries(RisLib INTERFACE PThreadLib)
-   endif()
-
-   target_link_libraries(${_target} RisLib)
+   target_link_libraries(${_target} boost_system)
+   target_link_libraries(${_target} boost_thread)
    
 endfunction()
 
-function(my_inc_import_RisLib _target)
+function(my_inc_import_boost _target)
 
-   target_include_directories(${_target} PUBLIC ${MyRisLibIncludePath})
+   target_include_directories(${_target} PUBLIC ${MyBoostIncludePath})
 
 endfunction()
 
