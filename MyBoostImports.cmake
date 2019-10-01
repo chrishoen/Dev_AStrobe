a96c216fc7c287959a5671ae58daa7a21d419baf
chrishoen <chris123hoen@gmail.com>
2019-10-01 14:56:03 -0400

    adding sqlite3

--- MyBoostImports.cmake
+++ MyBoostImports.cmake
@@ -8,10 +8,14 @@ function(my_init_boost_import_variables)
       set (MyBoostIncludePath "/usr/local/include/boost" PARENT_SCOPE)
       set (MyBoostSystemImportPath  "/usr/local/lib/libboost_system.a" PARENT_SCOPE)
       set (MyBoostThreadImportPath  "/usr/local/lib/libboost_thread.a" PARENT_SCOPE)
+      set (MySQLite3IncludePath "/usr/include" PARENT_SCOPE)
+      set (MySQLite3ImportPath "/usr/lib/arm-linux-gnueabihf/libsqlite3.a" PARENT_SCOPE)
    else()
       set (MyBoostIncludePath "C:/Beagle/boost/include" PARENT_SCOPE)
       set (MyBoostSystemImportPath  "C:/Beagle/boost/lib/libboost_system.a" PARENT_SCOPE)
       set (MyBoostThreadImportPath  "C:/Beagle/boost/lib/libboost_thread.a" PARENT_SCOPE)
+      set (MySQLite3IncludePath "C:/Beagle/sqlite3/include" PARENT_SCOPE)
+      set (MySQLite3ImportPath  "C:/Beagle/sqlite3/lib/libsqlite3.a" PARENT_SCOPE)
    endif()
 endfunction()
 
@@ -35,6 +39,26 @@ endfunction()
 function(my_inc_import_boost _target)
 
    target_include_directories(${_target} PUBLIC ${MyBoostIncludePath})
+
+endfunction()
+
+#*******************************************************************************
+#*******************************************************************************
+#*******************************************************************************
+
+function(my_lib_import_sqlite3 _target)
+
+   add_library(sqlite3 STATIC IMPORTED)
+
+   set_target_properties(sqlite3 PROPERTIES IMPORTED_LOCATION ${MySQLite3ImportPath})
+
+   target_link_libraries(${_target} sqlite3)
+   
+endfunction()
+
+function(my_inc_import_sqlite3 _target)
+
+   target_include_directories(${_target} PUBLIC ${MySQLite3IncludePath})
 
 endfunction()
 
