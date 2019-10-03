c193280451e3581bc8664f338fc180b7477f23a4
chrishoen <chris123hoen@gmail.com>
2019-10-03 15:56:50 -0400

    MyGlobalImports

--- MyGlobalImports.cmake
+++ MyGlobalImports.cmake
@@ -7,10 +7,13 @@ function(my_init_global_import_variables)
    if(NOT CMAKE_SYSTEM_VERSION EQUAL 101)
       set (MyRisLibIncludePath "/usr/local/include/RisLib" PARENT_SCOPE)
       set (MyRisLibImportPath  "/usr/local/lib/libRisLib.a" PARENT_SCOPE)
+      set (MyPThreadImportPath "/usr/lib/arm-linux-gnueabihf/libpthread.so" PARENT_SCOPE)
    else()
       set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib" PARENT_SCOPE)
       set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/libRisLib.a" PARENT_SCOPE)
+      set (MyPThreadImportPath "C:/Beagle/toolchain/arm-linux-gnueabihf/lib/libpthread.so" PARENT_SCOPE)
    endif()
+
 endfunction()
 
 #*******************************************************************************
@@ -20,26 +23,31 @@ endfunction()
 function(my_lib_import_RisLib _target)
 
    add_library(RisLib STATIC IMPORTED)
-
    set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
-
-   if (CMAKE_SYSTEM_VERSION EQUAL 101)
-      set (MyPThreadImportPath  "C:/Beagle/toolchain/arm-linux-gnueabihf/lib/libpthread.so")
-   else()
-      set (MyPThreadImportPath  "/usr/lib/arm-linux-gnueabihf/libpthread.so")
-   endif()
+   target_link_libraries(${_target} RisLib)
+   
    add_library(PThreadLib SHARED IMPORTED)
    set_target_properties(PThreadLib PROPERTIES IMPORTED_LOCATION ${MyPThreadImportPath})
    target_link_libraries(RisLib INTERFACE PThreadLib)
 
-   target_link_libraries(${_target} RisLib)
-   
 endfunction()
 
 function(my_inc_import_RisLib _target)
 
    target_include_directories(${_target} PUBLIC ${MyRisLibIncludePath})
 
+endfunction()
+
+#*******************************************************************************
+#*******************************************************************************
+#*******************************************************************************
+
+function(my_lib_import_pthread _target)
+
+   add_library(RisLib STATIC IMPORTED)
+   set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
+   target_link_libraries(${_target} RisLib)
+   
 endfunction()
 
 #*******************************************************************************
