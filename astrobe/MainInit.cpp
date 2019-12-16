5ae97832c9d6c823db6ffb94bbada410a051ae08
chrishoen <chris123hoen@gmail.com>
2019-12-16 16:44:21 -0500

    changed dir to /opt/prime

--- astrobe/MainInit.cpp
+++ astrobe/MainInit.cpp
@@ -25,7 +25,7 @@ void main_initialize(int argc,char** argv)
    // Set program directory.
 
    // Set the base directory global variable to the cproc directory path.
-   Ris::setBaseDirectory("/opt/aproc");
+   Ris::setBaseDirectory("/opt/prime");
 
    // Set the program working directory to the cproc directory path.
    Ris::chdirToBaseDirectory();
