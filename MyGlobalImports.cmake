10c7b5ddcb5ee82091fe88080287efd732165d4f
chrishoen <chris123hoen@gmail.com>
2020-04-13 21:56:39 -0400

    cmake build stuff

--- MyGlobalImports.cmake
+++ MyGlobalImports.cmake
@@ -4,14 +4,21 @@
 
 function(my_init_global_import_variables)
 
-   if(NOT CMAKE_SYSTEM_VERSION EQUAL 101)
+   if(MSVC)
+      set (MyRisLibIncludePath "C:\\MyTools\\MyLib\\include\\RisLib" PARENT_SCOPE)
+      set (MyRisLibImportPath  "C:\\MyTools\\MyLib\\lib\\RisLib.lib" PARENT_SCOPE)
+      set (MyDspLibIncludePath "C:\\MyTools\\MyLib\\include\\DspLib" PARENT_SCOPE)
+      set (MyDspLibImportPath  "C:\\MyTools\\MyLib\\lib\\DspLib.lib" PARENT_SCOPE)
+   elseif(NOT CMAKE_SYSTEM_VERSION EQUAL 101)
       set (MyRisLibIncludePath "/usr/local/include/RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "/usr/local/lib/libRisLib.a" PARENT_SCOPE)
-      set (MyPThreadImportPath "/usr/lib/arm-linux-gnueabihf/libpthread.so" PARENT_SCOPE)
+      set (MyRisLibImportPath  "/usr/local/lib/libRisLib.so" PARENT_SCOPE)
+      set (MyDspLibIncludePath "/usr/local/include/DspLib" PARENT_SCOPE)
+      set (MyDspLibImportPath  "/usr/local/lib/libDspLib.a" PARENT_SCOPE)
    else()
       set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/libRisLib.a" PARENT_SCOPE)
-      set (MyPThreadImportPath "C:/Beagle/toolchain/arm-linux-gnueabihf/lib/libpthread.so" PARENT_SCOPE)
+      set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/libRisLib.so" PARENT_SCOPE)
+      set (MyDspLibIncludePath "C:/MyTools/MyLib/include/DspLib" PARENT_SCOPE)
+      set (MyDspLibImportPath  "C:/MyTools/MyLib/lib/libDspLib.a" PARENT_SCOPE)
    endif()
 
 endfunction()
@@ -22,13 +29,17 @@ endfunction()
 
 function(my_lib_import_RisLib _target)
 
-   add_library(RisLib STATIC IMPORTED)
-   set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
-   target_link_libraries(${_target} RisLib)
-   
-   add_library(PThreadLib SHARED IMPORTED)
-   set_target_properties(PThreadLib PROPERTIES IMPORTED_LOCATION ${MyPThreadImportPath})
-   target_link_libraries(RisLib INTERFACE PThreadLib)
+   if (MSVC)
+      add_library(RisLib STATIC IMPORTED)
+      set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
+      target_link_libraries(RisLib INTERFACE ws2_32)
+      target_link_libraries(RisLib INTERFACE winmm)
+      target_link_libraries(${_target} RisLib)
+   else()
+      add_library(RisLib SHARED IMPORTED)
+      set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
+      target_link_libraries(${_target} RisLib)
+   endif()
 
 endfunction()
 
@@ -42,12 +53,18 @@ endfunction()
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_lib_import_pthread _target)
+function(my_lib_import_DspLib _target)
 
-   add_library(RisLib STATIC IMPORTED)
-   set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
-   target_link_libraries(${_target} RisLib)
-   
+   add_library(DspLib STATIC IMPORTED)
+   set_target_properties(DspLib PROPERTIES IMPORTED_LOCATION ${MyDspLibImportPath})
+   target_link_libraries(${_target} DspLib)
+
+endfunction()
+
+function(my_inc_import_DspLib _target)
+
+   target_include_directories(${_target} PUBLIC ${MyDspLibIncludePath})
+
 endfunction()
 
 #*******************************************************************************
